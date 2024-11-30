72b2a19f8e6bc3b2a343886a7915079dc546877e
Claire Felt <cfeltgungatan@gmail.com>
2024-11-30 12:04:21 -0700

    Stuff, photos etc

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,22 @@ const theYear = new Date();
 console.log(theYear.getFullYear());
 document.querySelector('#year').textContent = theYear.getFullYear();
 
+// Get the button
+const backToTopButton = document.getElementById("backToTop");
+
+// Show the button when scrolling down
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+// Scroll to top when the button is clicked
+backToTopButton.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Smooth scroll
+    });
+};
